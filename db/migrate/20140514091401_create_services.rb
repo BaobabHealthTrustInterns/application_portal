5ff32f26659b4fce3ae502ac79963355a8176ab5
Ralph B. Tambala <battleaccent@gmail.com>
2014-05-15 22:37:02 +0200

    Changed service attribute name

--- db/migrate/20140514091401_create_services.rb
+++ db/migrate/20140514091401_create_services.rb
@@ -1,7 +1,7 @@
 class CreateServices < ActiveRecord::Migration
   def self.up
     create_table :services do |t|
-      t.string "service_name", :null => false
+      t.string "name", :null => false
       t.string "url_path", :null => false
 
       t.timestamps

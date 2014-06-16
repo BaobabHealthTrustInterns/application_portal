aa6164033c1766c8c7161b7ed5fcf60667c2520f
Ralph Tambala <ralphbtambala@gmail.com>
2014-06-16 18:40:07 -0400

    Resolving secret_token issue

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApplicationPortal::Application.config.secret_token = 'c974bb32d741855c602969ceaf1ed3a502a156faf60e999f5270e7941947b8ccd30f17876fdb2e8aede758d368e62ee7a328652c'

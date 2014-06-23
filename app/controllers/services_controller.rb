274359a15d001a6328e4d5e5a86915336aa48a52
Mwanza <mwanza@ubuntu.ubuntu-domain>
2014-06-23 22:16:45 +0200

    i have made changes to services controller

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,14 +1,19 @@
 class ServicesController < ApplicationController
+	
   def new
+	@service = Service.new
   end
 
   def show
+	
   end
 
   def edit
   end
 
   def list
+	@service_names = Service.order("services.name ASC")
+	@services = Service.all
   	@page = "more"
   	
   	services = Service.limit(16)
@@ -21,13 +26,13 @@ class ServicesController < ApplicationController
 
   	if Service.count > 16
   		@nav = "More services >>"
-  	else
-  		@nav = ""
+	else
+		@nav = ""
   	end
   end
 
   def more
-  	@nav = "Previous services >>"
+  	@nav = "more"
   	@page = "list"
 
   	services = Service.all
@@ -37,9 +42,21 @@ class ServicesController < ApplicationController
   	@row_4 = services[28..31] || Hash.new
 
   	@rows = [@row_1, @row_2, @row_3, @row_4]
-
+	
   	@services = services[16..33]
   	render('list')
   end
-
+  
+  def create
+       
+        @service = Service.new(params[:service])
+        
+	if @service.save
+        redirect_to (:action=> 'list')
+        
+	end
+  end  
+   def hit_counts
+        
+   end
 end
